a52572c56de680211077d41140d8655d3ff6a4bf
yudapratamaa99 <yudapratamaa1307@gmail.com>
2026-05-07 00:56:04 +0700

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+const scrollUp = document.querySelector(".scroll-up");
+window.onscroll = () => {
+    if (window.scrollY > 700){
+        scrollUp.classList.add("scroll-active")
+    }else{
+        scrollUp.classList.remove("scroll-active")
+    }
+}
